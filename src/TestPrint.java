79ca3e47d83fbec771a04607af378896ad92c05d
radumbaciu <radumbaciu@gmail.com>
2022-07-15 20:39:02 +0300

    initial commit

---
+++ src/TestPrint.java
@@ -0,0 +1,15 @@
+public class TestPrint {
+
+    public static void print(Integer i) {
+        System.out.println("Integer");
+    }
+
+    public static void print(int i) {
+        System.out.println("int");
+    }
+
+    public static void print(long i) {
+        System.out.println("long");
+    }
+
+}
